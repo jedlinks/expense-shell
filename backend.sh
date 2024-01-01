943340b44511ddd31f44b55ed2984d154a98a06b
jedlinks <jedlinks@gmail.com>
2023-12-31 21:10:58 -0600

    Server info modification

--- backend.sh
+++ backend.sh
@@ -8,35 +8,35 @@ echo -e "\e[36m$*\e[0m"
 Head Disabling Nodejs
 dnf module disable nodejs -y &>>log_file
 
-Head " Enabling Nodejs:18"
+Head Enabling Nodejs:18
 dnf module enable nodejs:18 -y &>>log_file
 
-Head " Insatalling Nodejs"
+Head Insatalling Nodejs
 dnf install nodejs -y &>>log_file
 
-Head " Downloading Backend Content"
+Head Downloading Backend Content
 cp backend.service /etc/systemd/system/backend.service &>>log_file
 
-Head " Adding User"
+Head Adding User
 useradd expense &>>log_file
 
-Head " Deleting Application Files and Installing Content"
+Head Deleting Application Files and Installing Content
 rm -rf /app &>>log_file
 mkdir /app &>>log_file
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
 cd /app &>>log_file
 unzip /tmp/backend.zip &>>log_file
 
-Head " Insatalling Dependencies"
+Head Insatalling Dependencies
 npm install &>>log_file
 
-Head " Reloading and Restarting Backend"
+Head Reloading and Restarting Backend
 systemctl daemon-reload &>>log_file
 systemctl enable backend &>>log_file
 systemctl restart backend &>>log_file
 
-Head " Installing MYSQL"
+Head Installing MYSQL
 dnf install mysql -y &>>log_file
 
-Head " Loading Schema"
+Head Loading Schema
 mysql -h mysql-dev.jedlinks.com -uroot -p${MYSQL_PASSWORD} < /app/schema/backend.sql  &>>log_file
