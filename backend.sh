cb62cb23b2a30c96fd274483efebf23935f231a7
jedlinks <jedlinks@gmail.com>
2023-12-31 21:01:48 -0600

    Server info modification

--- backend.sh
+++ backend.sh
@@ -1,37 +1,42 @@
 MYSQL_PASSWORD=$1
 log_file=/tmp/expense.log
-echo -e "\e[33m Disabling Nodejs\e[0m"
+
+Head() {
+echo -e "\e[33m$1\e[0m"
+}
+
+echo -e "\e[33m Disabling Nodejs"
 dnf module disable nodejs -y &>>log_file
 
-echo -e "\e[33m Enabling Nodejs:18\e[0m"
+Head " Enabling Nodejs:18"
 dnf module enable nodejs:18 -y &>>log_file
 
-echo -e "\e[33m Insatalling Nodejs\e[0m"
+Head " Insatalling Nodejs"
 dnf install nodejs -y &>>log_file
 
-echo -e "\e[33m Downloading Backend Content\e[0m"
+Head " Downloading Backend Content"
 cp backend.service /etc/systemd/system/backend.service &>>log_file
 
-echo -e "\e[33m Adding User\e[0m"
+Head " Adding User"
 useradd expense &>>log_file
 
-echo -e "\e[33m Deleting Application Files and Installing Content\e[0m"
+Head " Deleting Application Files and Installing Content"
 rm -rf /app &>>log_file
 mkdir /app &>>log_file
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
 cd /app &>>log_file
 unzip /tmp/backend.zip &>>log_file
 
-echo -e "\e[33m Insatalling Dependencies\e[0m"
+Head " Insatalling Dependencies"
 npm install &>>log_file
 
-echo -e "\e[33m Reloading and Restarting Backend\e[0m"
+Head " Reloading and Restarting Backend"
 systemctl daemon-reload &>>log_file
 systemctl enable backend &>>log_file
 systemctl restart backend &>>log_file
 
-echo -e "\e[33m Installing MYSQL\e[0m"
+Head " Installing MYSQL"
 dnf install mysql -y &>>log_file
 
-echo -e "\e[33m Loading Schema\e[0m"
+Head " Loading Schema"
 mysql -h mysql-dev.jedlinks.com -uroot -p${MYSQL_PASSWORD} < /app/schema/backend.sql  &>>log_file
