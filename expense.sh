adb25773764fa0167eec0fbcf3a8c9bf8351553c
jedlinks <jedlinks@gmail.com>
2023-12-31 21:51:54 -0600

    Server info modification

--- expense.sh
+++ expense.sh
@@ -5,28 +5,28 @@ echo -e "\e[36m$*\e[0m"
 }
 
 Head Installing Nginx
-dnf install nginx -y
+dnf install nginx -y &>>log_file
 echo $?
 
 Head Copying Application Contents
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
 echo $?
 
 Head Removing Old/Default Contens
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>log_file
 echo $?
 
 Head Downloading Application Contents
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
 echo $?
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>log_file
 
 Head Installing Frontend
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>log_file
 echo $?
 
 Head Enabling and Starting Nginx
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>log_file
+systemctl restart nginx &>>log_file
 echo $?
