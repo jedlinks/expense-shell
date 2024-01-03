8d8deaa8c7b3ea45532e85be3efe52a6ee1c226a
jedlinks <jedlinks@gmail.com>
2024-01-03 10:08:15 -0600

    Server info modification

--- Mysql.sh
+++ Mysql.sh
@@ -8,16 +8,21 @@ source common.sh
 
 Head Disabling MYSQL
 dnf module disable mysql -y &>>log_file
+Stat $?
 
 Head Copying Repo Files
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file
+Stat $?
 
 Head Installing Mysql Community Server
 dnf install mysql-community-server -y &>>log_file
+Stat $?
 
 Head Enabling and starting MYSQL
 systemctl enable mysqld
 systemctl restart mysqld
+Stat $?
 
 Head Inputing Password
 mysql_secure_installation --set-root-pass ${MYSQL_PASSWORD} &>>log_file
+Stat $?
