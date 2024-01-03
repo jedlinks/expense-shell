742fd5be0603df2f3b6a1305524074f1741afa34
jedlinks <jedlinks@gmail.com>
2024-01-03 08:30:17 -0600

    Server info modification

--- common.sh
+++ common.sh
@@ -4,3 +4,12 @@ log_file=/tmp/expense.log
 Head() {
 echo -e "\e[36m$*\e[0m"
 }
+
+Stat() {
+  if [ "$1" -eq 0 ]; then
+     echo SUCCESS
+  else
+     echo FAILURE
+     exit 1
+  fi
+  }
--- expense.sh
+++ expense.sh
@@ -1,27 +1,27 @@
 source common.sh
 Head Installing Nginx
 dnf install nginx -y &>>log_file
-echo $?
+Stat $?
 
 Head Copying Application Contents
 cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
-echo $?
+Stat $?
 
 Head Removing Old/Default Contens
 rm -rf /usr/share/nginx/html/* &>>log_file
-echo $?
+Stat $?
 
 Head Downloading Application Contents
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
-echo $?
+Stat $?
 
 cd /usr/share/nginx/html &>>log_file
 
 Head Installing Frontend
 unzip /tmp/frontend.zip &>>log_file
-echo $?
+Stat $?
 
 Head Enabling and Starting Nginx
 systemctl enable nginx &>>log_file
 systemctl restart nginx &>>log_file
-echo $?
+Stat $?
